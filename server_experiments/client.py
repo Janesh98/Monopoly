526ed65d2e2709331bfd8cfb987ad69258ebcbf8
Rafal Domanski <rafal.domanski2@mail.dcu.ie>
2019-11-13 21:13:19 +0000

    Created a player object and added simple commands

--- server_experiments/client.py
+++ server_experiments/client.py
@@ -2,34 +2,112 @@ import socket
 import select
 import sys
 
-class Player():
-    def __init__(self, player_id, player_name=None, pos=None):
-        self.player_id = player_id
-        self.name = player_name
-        self.pos = pos
+'''
+name <your playr name>          #sets name of player
+move <int> <int>                #change position on a 2D grid
+list                            #display all local player objects
+echo <message>                  #echo back a message
+address                         #get your IP address
+'''
 
+#setup server connection
 ip_address = "127.0.0.1"
 port = 8001
 
 server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_connection.connect((ip_address,port))
-print("client connected")
+
+class Player():
+    def __init__(self, player_id, player_name=None, pos=None):
+        self.name = player_name
+        self.player_id = player_id
+        self.is_ready = False           #used for starting game when all players are ready
+        self.is_turn = False            #is it this players turn?
+        if pos == None:
+            self.pos = [0, 0]
+        else:
+            self.pos = pos
+    
+    def name(self, name):                   #sets name of a player
+        self.name = name
+    
+    def move(self, new_pos):                #move a player on a grid
+        tmp = new_pos.split()
+        for i in range(len(self.pos)):
+            self.pos[i] = int(tmp[i])
+    
+    def client_command(self, command):      #process commands from server
+        tokens = command.strip().split(" ", 1)
+        try:
+            if tokens[0] in self.commands_nullary.keys():
+                self.commands_nullary[tokens[0]](self)
+            elif tokens[0] in self.commands_unary.keys():
+                self.commands_unary[tokens[0]](self,tokens[1])
+            else:
+                print("Command from server not recognised: '{}'".format(tokens[0]))     #should never happen
+        except IndexError:
+            print("Server input error at: '{}'".format(tokens[0]))                      #should never happen
+    
+    def __str__(self):          #returns a formated information about player
+        return ("name: {}\n"
+                "id: {}\n"
+                "pos: {}").format(self.name, self.player_id, self.pos)
+    
+    commands_nullary = {}
+        
+    commands_unary = {
+        "move": move,
+        "name": name,
+    }
+
+local_player_id = None      #current clients player id
+players = {}                #dictionary of players by player_id
+
+
+def server_input(commands):
+    for command in commands.strip().split("\n"):
+        tokens = command.strip().split(" ", 1)
+        if tokens[0].isdigit():                                          #if player id given as first arg
+            int_id = int(tokens[0])
+            if len(tokens) > 1:
+                if int_id in players.keys():                             #if player exists
+                    players[int_id].client_command(tokens[1])            #do command in a player object
+                else:
+                    players[int_id] = Player(int_id)                           #else make a new player and do command
+                    players[int_id].client_command(tokens[1])
+            else:
+                players[int_id] = Player(int_id)                               #if only id given, make new player
+        elif tokens[0] == "print":          #print to client
+            print(tokens[1])
+        elif tokens[0] == "id":             #set local_player_id to conn adderss id
+            local_player_id = int(tokens[1])
+        else:
+            print("unrecognised server command: '{}'".format(command))
 
 inputs = [sys.stdin, server_connection]
 
+#main loop
 while True:
     try:
         read, write, error = select.select(inputs, [], [])
         for data_input in read:
-            if data_input == server_connection:
+            if data_input == server_connection:         #if server sent message
                 message = data_input.recv(1024)
                 parsed_message = message.decode()
                 if parsed_message != '':
-                    print(parsed_message)
+                    server_input(parsed_message)        #processes command from server
+                #    print(parsed_message)
             else:
                 message = sys.stdin.readline()
-                server_connection.send(message.strip().encode())
-                sys.stdout.flush()
+                if message.strip() == "list":           #if command = "list", display players
+                    #display all players
+                    print("#"*20)
+                    for p in sorted(players.values(), key=lambda x:x.player_id):
+                        print(p)
+                        print("-"*20)
+                else:                                   #otherwise send command to server
+                    server_connection.send(message.strip().encode())
+                    sys.stdout.flush()
             
     except KeyboardInterrupt:
         server_connection.close()
--- server_experiments/server.py
+++ server_experiments/server.py
@@ -2,12 +2,19 @@ import socket
 import threading
 import sys
 
+'''
+The server can send commands to clients in a format:
+"<player_id> <command> <args>" or "<print/id> <string/digits>\n"
+commands: print, id, move, name
+'''
 
 class Player():
-    def __init__(self, conn, player_id=0, player_name=None, pos=None):
+    def __init__(self, conn, player_name=None, pos=None):
         self.conn = conn
-        self.player_id = player_id
         self.name = player_name
+        self.player_id = get_addr(conn)[1]
+        self.is_ready = False           #used for starting game when all players are ready
+        self.is_turn = False            #is it this players turn?
         if pos == None:
             self.pos = [0, 0]
         else:
@@ -15,53 +22,62 @@ class Player():
     
     def name(self, name):                   #sets name of a player
         self.name = name
-        message = "Name set to: " + name
-        send_message(conn, message)
-        print(message)
+        message = "{} name {}\n".format(self.player_id, name)
+        broadcast(message)
+        print(message, endl="")
     
     def move(self, new_pos):                #move a player on a grid
-        if type(new_pos) == str:            #moving from string "num num"
-            tmp = new_pos.split()
-            for i in range(len(self.pos)):
-                self.pos[i] = int(tmp[i])
-        else:                               #moving from a tuple (int, int)
-            for i in range(len(self.pos)):
-                self.pos[i] = new_pos[i]
+        tmp = new_pos.split()
+        for i in range(len(self.pos)):
+            self.pos[i] = int(tmp[i])
         
-        message = "{} moved to: {}".format(self.name, self.pos)
+        message = "{} move {}\n".format(self.player_id, new_pos)
         broadcast(message)
-        print(message)
+        print(message, endl="")
     
     def client_command(self, command):
-        tokens = command.strip().split(" ", 1)
+        print(self.player_id, command)                          #prints command sent
+        tokens = command.strip().split(" ", 1)                  #splits on first space
         try:
-            if tokens[0] in self.commands_nullary.keys():
+            if tokens[0] in self.commands_nullary.keys():       #checks if the first word is a command
                 self.commands_nullary[tokens[0]](self)
             elif tokens[0] in self.commands_unary.keys():
                 self.commands_unary[tokens[0]](self,tokens[1])
             else:
-                message = "Command not recognised: '{}'".format(tokens[0])
+                #if command not recognised, send message
+                message = "print Command not recognised: '{}'\n".format(tokens[0])
                 send_message(self.conn, message)
-                print(message)
+                print(message, endl="")
         except IndexError:
-            message = "Wrong use of command: '{}'".format(tokens[0])
+            #if used wrong arguments, send message
+            message = "print Wrong use of command: '{}'\n".format(tokens[0])
             send_message(self.conn, message)
-            print(message)
+            print(message, endl="")
     
-    def echo_addr(self):
-        send_message(self.conn, "Your address: {}".format(get_addr(self.conn)))
+    def echo_addr(self):        #sends clients address
+        send_message(self.conn, "print Your address: {}\n".format(get_addr(self.conn)))
     
+    def echo(self, message):    #echos back message
+        send_message(self.conn, "print {}\n".format(message))
+    
+    def info(self):             #returns commands setting up a player class
+        return "{0} name {1}\n{0} move {2} {3}\n".format(self.player_id, self.name, self.pos[0], self.pos[1])
+        
+    #dictionaried used in client_command
     commands_nullary = {
         "address": echo_addr
     }
         
     commands_unary = {
         "move": move,
-        "name": name
+        "name": name,
+        "echo": echo
     }
 
 
-ip_addr = "127.0.0.1"
+#socket setup
+
+ip_addr = ""
 port = 8001
 
 serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -74,15 +90,21 @@ serversocket.listen(6)
 
 connected_devices = {}
 
-def start_client_thread(conn,address):
+def send_server_info(conn):             #used for setting up the client to the servers current state
+    server_info = ""
+    for c, dic in connected_devices.items():
+        server_info += dic["player"].info()     #gets info about every player
+    send_message(conn, server_info)
+    print("Sent server info to {}".format(dic["addr"]))
+
+def start_client_thread(conn,address):          #starts client thread that listens to a client
     th = threading.Thread(target=client_thread ,args=(conn,address))
     th.start()
     connected_devices[conn]["thread"] = th
-    connected_devices[conn]["player"] = Player(conn, )
-    
-    #for c in connected_devices
+    connected_devices[conn]["player"] = Player(conn)    #creates player object
     
 
+#all send and broadcast messages must end with a "\n" in case 2 packets combine
 def broadcast(message, original_conn=None):
     for conn in connected_devices:
         if conn != original_conn:
@@ -96,36 +118,36 @@ def get_addr(conn):
     
 
 def client_thread(conn, addr):
-    welcome = "Welcome, to input your name type 'name <your name>'"
-    conn.send(welcome.encode())
-
+    #broadcast to other clients that a new player joined
+    broadcast(str(addr[1]) + "\n")
+    
+    #send server info (setup client)
+    send_server_info(conn)
+    #welcome message and client id
+    send_message(conn, "print Welcome, input your name 'name <your name>'\nid {}\n".format(addr[1]))
+    
     while server_running:
         try:
-            message = conn.recv(1024)
+            message = conn.recv(1024)           #receive client commands
             if message:
                 enc_message = message.decode()
-                print(enc_message)
-                connected_devices[conn]["player"].client_command(enc_message)
-                '''
-                message_to_send = "<{}> {}".format(addr, enc_message.rstrip("\n"))
-                print(message_to_send)
-                broadcast(message_to_send, conn)
-                '''
+                connected_devices[conn]["player"].client_command(enc_message)   #process client commands
             else:
-                pass	
+                pass
         except:
-            continue	
+            pass
 
+#main loop
 try:
     while True:
-        conn , addr = serversocket.accept()
-        connected_devices[conn] = {"addr" :addr}
+        conn , addr = serversocket.accept()         #accept new client
+        connected_devices[conn] = {"addr" :addr}    #put into a dictionary
         print("{} connected".format(addr))
         start_client_thread(conn, addr)
-except KeyboardInterrupt:
+except KeyboardInterrupt:           #shutdown server by ^C
     print(" Server shutting down")
     for conn in connected_devices:
-        conn.close()
+        conn.close()                #closes all connections
     serversocket.close()
     server_running = False
     sys.exit(0)
