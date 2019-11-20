f384214d1b2e177dabcb927e6e830338a4ae366e
Rafal Domanski <rafal.domanski2@mail.dcu.ie>
2019-11-20 17:14:31 +0000

    set up server and client classes

---
+++ code/client.py
@@ -0,0 +1,61 @@
+import socket
+import select
+import sys
+
+class Client():
+    def __init__(self, address, port):
+        self.address = address
+        self.port = int(port)
+        self.connected = False
+        self.server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def establish_connection(self, address, port):
+        try:
+            self.server_connection.connect((address, port))
+            self.connected = True
+        except ConnectionRefusedError:
+            print("Couldin't connect to server")
+    
+    def send_server(self, message):
+        self.server_connection.send(message.strip().encode())
+    
+    def run(self):
+    
+        self.establish_connection(self.address, self.port)
+        
+        ##########  SETUP TKINTER HERE  #############
+        
+        #sys.stdin temporary, use tkinter instead
+        inputs = [sys.stdin, self.server_connection]
+        
+        while self.connected:       #Main game loop
+        
+            ######  DISPLAY TKINTER HERE  ########
+            
+            #asking for input, server or tkinter
+            try:
+                read, write, error = select.select(inputs, [], [])
+                for data_input in read:
+                    if data_input == self.server_connection:         #if server sent message
+                        message = data_input.recv(2048)
+                        parsed_message = message.decode()
+                        if parsed_message != '':
+                            print(parsed_message)   #print message from server, temporary
+                            #
+                            #display.process_command(parsed_message)        #processes command from server
+                            #
+                    else:
+                        #
+                        #Input from tkinter gets sent to server
+                        #
+                        self.send_server(sys.stdin.readline())
+                        sys.stdout.flush()
+                    
+            except KeyboardInterrupt:
+                self.server_connection.close()
+                self.connected = False
+                print("disconnected")
+
+if __name__ == "__main__":
+    c = Client(sys.argv[1], sys.argv[2])
+    c.run()
---
+++ code/server.py
@@ -0,0 +1,72 @@
+import socket
+import threading
+import sys
+
+class Server():
+    def __init__(self, port="8000", players=6):
+        self.ip_addr = ""
+        self.port = int(port)
+        self.max_players = players
+        self.connected_devices = {}
+        self.server_running = True
+        
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+        self.server_socket.bind((self.ip_addr, self.port))
+        self.server_socket.listen(players)
+        
+    def send_client(self, conn, message):
+        conn.send(message.strip().encode())
+        
+    def send_all(self, message):
+        for conn in self.connected_devices.keys():
+            self.send_client(conn, message)
+        
+    def start_client_thread(self, conn, address):          #starts client thread that listens to a client
+        th = threading.Thread(target=self.client_thread, args=(conn, address))
+        th.start()
+        self.connected_devices[conn]["thread"] = th
+        #self.connected_devices[conn]["player"] = Player(conn)    #creates player object
+    
+    def client_thread(self, conn, addr):        #each thread connects to each client
+        #broadcast to other clients that a new player joined
+        #broadcast(str(addr[1]) + "\n")
+        
+        #send server info (setup client)
+        #send_server_info(conn)
+        #welcome message and client id
+        #send_message(conn, "print Welcome, input your name 'name <your name>'\nid {}\n".format(addr[1]))
+        
+        while self.server_running:
+            try:
+                message = conn.recv(2048)           #receive client commands
+                if message:
+                    enc_message = message.decode()
+                    self.send_all(enc_message)      #echos message
+                    print(enc_message)              #for debugging
+                    #
+                    #game.process_command(enc_message)
+                    #
+                    #connected_devices[conn]["player"].client_command(enc_message)   #process client commands
+                else:
+                    pass
+            except:
+                pass
+
+    def run(self):
+        try:
+            while True:
+                conn , addr = self.server_socket.accept()         #accept new client
+                self.connected_devices[conn] = {"addr" :addr}    #put into a dictionary
+                print("{} connected".format(addr))
+                self.start_client_thread(conn, addr)
+        except KeyboardInterrupt:           #shutdown server by ^C
+            print(" Server shutting down")
+            for conn in self.connected_devices:
+                conn.close()                #closes all connections
+            self.server_socket.close()
+            self.server_running = False
+
+if __name__ == "__main__":
+    s = Server(port=sys.argv[1])
+    s.run()
