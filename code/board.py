9bb608676884be7c94da622d3656fc0c76fe3a2a
Arturas Bulcenka <arturas.bulcenka2@mail.dcu.ie>
2019-11-28 14:49:23 +0000

    Update board.py

--- code/board.py
+++ code/board.py
@@ -1,16 +1,30 @@
+import random
+
 class Board:
 
-    def __init__(self, pawn_list, chance_cards, comm_chest_cards):
-
+    def __init__(self, pawn_list, property_list, chance_cards, comm_chest_cards):
         ''' initializes:
             pawn_list : list of different types of pawns (Pawn[])
-            chance_cards : list of different types of chance cards (str[])
-            comm_chest_cards : list of different community chest cards (str[])
+            property_list : list of different types of pawns (Properties[])
+            chance_cards : list of different types of chance cards (Chance[])
+            comm_chest_cards : list of different community chest cards (Comm[])
         '''
-        pass
+        self.pawn_list = pawn_list
+        self.property_list = property_list
+        self.chance_cards = chance_cards["yes","no"]
+        self.comm_chest_cards = comm_chest_cards
 
-    def random_card(self, card_type):
+    def random_chance_card(self, chance_cards):
 
-        ''' returns a random card from the list of the requested type
-            return (str)
+        ''' returns a random card from the list of the chance type
+            return (card)
         '''
+        return random.choice(chance_cards)
+
+N
+     def random_comm_card(self, card_type):
+
+        ''' returns a random card from the list of the community chest type
+            return (card)
+        '''
+        return random.choice(comm_chest_cards)
