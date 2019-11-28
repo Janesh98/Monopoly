3f2382bee9941c8c326a8511450520a139d08536
Paul McNally <paul.mcnally5@mail.dcu.ie>
2019-11-28 13:45:46 +0000

    pawn class

--- code/pawn.py
+++ code/pawn.py
@@ -1,17 +1,8 @@
 class Pawn:
 
     def __init__(self, colour, position):
-
-        ''' initializes the pawn selected by the user
-            and applies the selected colour
-            colour : (str)
-            position : (int)
-        '''
-        pass
+        self.colour = colour
+        self.position = position
 
     def move(self, position):
-
-        ''' updates the position of the pawn on the board
-            return (int)
-        '''
-        pass
+        self.position = position
